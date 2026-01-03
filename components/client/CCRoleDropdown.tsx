ea5cdb5759d717fb259df4728be6365cb46b6659
phil002109 <vermontphil.paguiligan@gmail.com>
2026-01-04 02:59:18 +0800

    Chore: disable role dropdown

--- components/client/CCRoleDropdown.tsx
+++ components/client/CCRoleDropdown.tsx
@@ -25,12 +25,13 @@ export default function CCRoleDropdown({role} : {role: string}) {
   }
 
   return (
-    <select defaultValue={role} onChange={handleChange} className="bg-transparent text-slate-200 py-2 uppercase appearance-none text-center cursor-pointer focus:outline-none font-medium">
-      { category.ROLES.map((r) => (
-          // str.replace(/\d+/g, "") removes numbers in name
-          <option key={r} value={r} className={Object.values(params).includes(encodeURI(r)) ? "hidden" : ""}>{r.replace(/\d+/g, "")}</option>
-        ))
-      }
-    </select>
+    // <select defaultValue={role} onChange={handleChange} className="bg-transparent text-slate-200 py-2 uppercase appearance-none text-center cursor-pointer focus:outline-none font-medium">
+    //   { category.ROLES.map((r) => (
+    //       // str.replace(/\d+/g, "") removes numbers in name
+    //       <option key={r} value={r} className={Object.values(params).includes(encodeURI(r)) ? "hidden" : ""}>{r.replace(/\d+/g, "")}</option>
+    //     ))
+    //   }
+    // </select>
+    <div className="bg-transparent text-slate-200 py-2 uppercase appearance-none text-center focus:outline-none font-medium">{role}</div>
   )
 }
