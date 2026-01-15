ba7f0335619111bbb3d8f518f8ee659c59d5c7fb
phil002109 <vermontphil.paguiligan@gmail.com>
2026-01-15 23:18:50 +0800

    Chore: refactor event date retrieval to use getTodayDate helper function

--- app/api/events/upcoming/route.ts
+++ app/api/events/upcoming/route.ts
@@ -1,13 +1,13 @@
 import connectMongoDB from "@/libs/mongodb";
 import Event from "@/models/event";
+import { getTodayDate } from '@/utils/helpers';
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     await connectMongoDB();
-    const now = new Date();
-    now.setHours(0, 0, 0, 0); // Start of today
-    const events = await Event.find({ date: { $gte: now } }).sort({ date: 1 });
+    const today = getTodayDate();
+    const events = await Event.find({ date: { $gte: today } }).sort({ date: 1 });
     return NextResponse.json({ data: events }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
