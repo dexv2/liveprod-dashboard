0a98656fd8ad45211831c8b4c0c089f5c55ca77d
phil002109 <vermontphil.paguiligan@gmail.com>
2026-01-16 00:01:27 +0800

    Fix: update routes of api/events/[id] to api/events/id/[id] to avoid API confusion with api/events/upcoming

--- app/api/events/upcoming/route.ts
+++ app/api/events/upcoming/route.ts
@@ -7,10 +7,7 @@ export async function GET() {
   try {
     await connectMongoDB();
     const today = getTodayDate();
-    const events = await Event.find().sort({ date: 1 });
-    // const events = await Event.find({ date: { $gte: today } }).sort({ date: 1 });
-    console.log('today:', today);
-    console.log('events:', events);
+    const events = await Event.find({ date: { $gte: today } }).sort({ date: 1 });
     return NextResponse.json({ data: events }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ message: error.message }, { status: 500 });
