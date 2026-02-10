61866345d20b7b5eccfc56c520ba8082f1f9418f
Vermont <vermontphil.paguiligan@gmail.com>
2026-02-10 17:22:45 +0800

    Apply VIEW_VOLUNTEERS_LISTS and ADD_VOLUNTEER access permissions

--- components/server/SCAllVolunteers.tsx
+++ components/server/SCAllVolunteers.tsx
@@ -1,6 +1,5 @@
 import dynamic from 'next/dynamic';
 import { getAllVolunteers } from '@/utils/apis/get';
-import { checkAdminAuth } from '@/utils/helpersServer';
 import GCLoading from '../global/GCLoading';
 const CCAllVolunteers = dynamic(() => import('@/components/client/CCAllVolunteers'), {
   ssr: false,
@@ -9,12 +8,11 @@ const CCAllVolunteers = dynamic(() => import('@/components/client/CCAllVolunteer
 
 export default async function SCAllVolunteers() {
   const volunteers = await getAllVolunteers();
-  const isAdmin = await checkAdminAuth();
 
   // Handle case where volunteers data might be undefined or have errors
   const volunteerData = volunteers?.data || [];
 
 	return (
-    <CCAllVolunteers data={volunteerData} isAdmin={isAdmin} />
+    <CCAllVolunteers data={volunteerData} />
 	)
 }
