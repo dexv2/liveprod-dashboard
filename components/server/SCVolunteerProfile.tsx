0007898bb8b26b87dfd3c414e9f89a34693a78b1
Vermont <vermontphil.paguiligan@gmail.com>
2026-02-11 08:52:49 +0800

    Apply UPDATE_VOLUNTEER_PROFILE access permission

--- components/server/SCVolunteerProfile.tsx
+++ components/server/SCVolunteerProfile.tsx
@@ -1,25 +1,17 @@
 import CCVolunteerProfile from "@/components/client/CCVolunteerProfile";
 import { getVolunteerById } from "@/utils/apis/get";
-import { checkAuth, checkAdminAuth } from "@/utils/helpersServer";
 import { redirect } from "next/navigation";
 
 export default async function SCVolunteerProfile({ id }: { id: string }) {
   try {
-    const isAuthenticated = await checkAuth();
-    const isAdmin = await checkAdminAuth();
-
     // Add timestamp to force fresh data fetch
     const res = await getVolunteerById(id);
     const volunteer = res.data;
 
-    // Debug logging
-    console.log('Volunteer schedules:', volunteer.schedules);
-    console.log('Schedule services:', volunteer.schedules?.map((s: any) => s.service));
-
     // Allow access to volunteer profile even without authentication
     // This enables volunteer ID lookup functionality
     return (
-      <CCVolunteerProfile volunteer={volunteer} isAuthenticated={isAuthenticated} isAdmin={isAdmin} />
+      <CCVolunteerProfile volunteer={volunteer} />
     )
   } catch (error) {
     redirect("/volunteer/all");
