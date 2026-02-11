bfd6dd57db94a8b2c71d9a75b1a198f01cd8995c
Vermont <vermontphil.paguiligan@gmail.com>
2026-02-11 09:31:18 +0800

    Chore: remove redundant authentication and add typecheck

--- app/schedule/role/events/page.tsx
+++ app/schedule/role/events/page.tsx
@@ -1,18 +1,14 @@
 export const dynamic = "force-dynamic";
 
 import SCEventsManager from "@/components/server/SCEventsManager";
-import { auth } from "@/auth";
 import { Suspense } from 'react';
 import GCLoading from '@/components/global/GCLoading';
 
 export default async function EventsPage() {
-  const session = await auth();
-  const isAuthenticated = !!(session?.user as any)?.username;
-
   return (
     <div className="w-full">
       <Suspense fallback={<GCLoading />}>
-        <SCEventsManager isAuthenticated={isAuthenticated || false} />
+        <SCEventsManager />
       </Suspense>
     </div>
   );
