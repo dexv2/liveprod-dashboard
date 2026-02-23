81b7d85ffe7e018265bf08c9d739ca1bf9e9bb16
Vermont <vermontphil.paguiligan@gmail.com>
2026-02-23 18:12:56 +0800

    Change Sunday calendar schedule bg to green

--- components/client/CCCalendarSchedule.tsx
+++ components/client/CCCalendarSchedule.tsx
@@ -154,7 +154,7 @@ export default function CpCalendarSchedule({ events, length }: { events: any, le
                 </h3>
                 <div className="space-y-2">
                   {groupedEvents[dateKey].map((event: any, i: number) => (
-                    <div key={i} className={`${formatDayShort(dateKey) === "SAT" ? "border-sky-700" : "border-green-700" } border-l-4 pl-3 py-2 bg-sky-50 rounded`}>
+                    <div key={i} className={`${formatDayShort(dateKey) === "SAT" ? "border-sky-700 bg-sky-50" : "border-green-700 bg-green-50" } border-l-4 pl-3 py-2 rounded`}>
                       <div className="font-medium text-sm">{event.title}</div>
                       <div className="text-xs text-gray-600 mt-1">
                         {moment(event.start).format('h:mm A')}
