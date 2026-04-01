2918c6b49749fbcec37da200156eb1c4e890be5c
Vermont <vermontphil.paguiligan@gmail.com>
2026-04-01 18:39:17 +0800

    Fix volunteer name access in CCVolunteerCell component

--- components/client/CCVolunteerCell.tsx
+++ components/client/CCVolunteerCell.tsx
@@ -29,7 +29,7 @@ export default function CCVolunteerCell({ service, isSaturday = false }: { servi
   return (
     <td onClick={handleClick} className={`${hasAssignPermission && !!service?.id ? "hover:bg-slate-300 cursor-pointer" : ""} ${isSaturday ? "border-x-zinc-500" : "border-x-slate-300"} border-x min-w-12 lg:min-w-20 px-0.5 lg:px-1`}>
       <div className="flex flex-col justify-center overflow-hidden h-[1.2rem] lg:h-[1.4rem] text-center text-[10px] font-medium sm:font-normal md:text-sm lg:text-sm xl:text-sm">
-        {!!service?.id ? service?.volunteer?.[0]?.firstName : 'N/A'}
+        {!!service?.id ? service?.volunteer?.firstName : 'N/A'}
       </div>
     </td>
   );
