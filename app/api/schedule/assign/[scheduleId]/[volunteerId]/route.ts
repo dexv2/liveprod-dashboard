e017803e734bb08826367db95fdb5beab2721826
Vermont <vermontphil.paguiligan@gmail.com>
2026-04-01 18:41:47 +0800

    Add SNS Google sheet record

--- app/api/schedule/assign/[scheduleId]/[volunteerId]/route.ts
+++ app/api/schedule/assign/[scheduleId]/[volunteerId]/route.ts
@@ -1,7 +1,8 @@
 import connectMongoDB from "@/libs/mongodb";
 import Schedule from "@/models/schedule";
 import Volunteer from "@/models/volunteer";
-import { recordVolunteerToSheet } from '@/utils/gsheet';
+import { category } from '@/utils/constants';
+import { recordVolunteerToSheet, recordVolunteerToSheetSNS } from '@/utils/gsheet';
 import { NextResponse } from "next/server";
 
 export async function PUT(request: any, { params }: any) {
@@ -12,14 +13,19 @@ export async function PUT(request: any, { params }: any) {
 
     // Update the volunteer's schedules array
     const volunteer = await Volunteer.findByIdAndUpdate(volunteerId, { $push: { "schedules": scheduleId }});
-    if (schedule?.date && volunteer?.name) {
-      recordVolunteerToSheet(schedule.date, schedule.service, schedule.role, volunteer.name)
+    if (schedule?.date && volunteer?.name && category.SUNDAY_SERVICES.includes(schedule.service)) {
+      // Record to Sunday services Google Sheet schedule
+      recordVolunteerToSheet(schedule.date, schedule.service, schedule.role, volunteer.name);
+    } else if (schedule?.date && volunteer?.name && category.SATURDAY_SERVICES.includes(schedule.service)) {
+      // Record to SNS Google Sheet schedule
+      recordVolunteerToSheetSNS(schedule.date, schedule.service, schedule.role, volunteer.name);
     }
 
     // remove the schedule from previous volunteer if it exists
     await Volunteer.findByIdAndUpdate(schedule.volunteer, { $pullAll: { "schedules": [scheduleId] }});
     return NextResponse.json({message: "Schedule assigned to volunteer successfully!"}, {status: 200});
   } catch (error: any) {
+    console.error(`Failed to assign schedule to volunteer: ${error}`);
     return NextResponse.json({message: error.message}, {status: 500});
   }
 }
